80d84717babd6458f4ed392b9e8a837c5780f090
cardosoa2 <72450670+cardosoa2@users.noreply.github.com>
2021-01-20 22:09:25 +0000

    Genesis

---
+++ DataStoreService/src/main/java/dev/midas/dss/Main.java
@@ -0,0 +1,8 @@
+package dev.midas.dss;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello DSS");
+    }
+}
---
+++ xBrokerConnector/src/main/java/dev/midas/xbc/Main.java
@@ -0,0 +1,8 @@
+package dev.midas.xbc;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello XBC");
+    }
+}
